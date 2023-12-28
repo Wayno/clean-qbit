1bf8d37794c078e58c86248bd7ed9e29e602eb1d
Wayne <841215+Wayno@users.noreply.github.com>
2023-12-28 12:36:26 +0800

    Update clean-qbit.py

--- clean-qbit.py
+++ clean-qbit.py
@@ -4,40 +4,34 @@ import time
 import shutil
 
 # qBittorrent server information
-qbt_host = 'http://localhost:8080/'  # Change this to your qBittorrent server URL with port 9998
+qbt_host = 'http://localhost:9998/'  # Change this to your qBittorrent server URL with port 9998
 qbt_username = 'admin'               # Change this to your qBittorrent username
-qbt_password = 'password'           # Change this to your qBittorrent password
+qbt_password = 'password1'           # Change this to your qBittorrent password
 
-# Directory path to check for files/folders
+# Directory path to check for folders
 download_directory = 'D:/Completed/sonarr-tv'  # Change to your desired directory
 
-def get_qbittorrent_downloads():
+def get_active_qbittorrent_folders():
     qbt_client = qbittorrentapi.Client(host=qbt_host, username=qbt_username, password=qbt_password)
 
     # Retrieve the list of all torrents in qBittorrent
     torrents = qbt_client.torrents.info()
 
-    # Extract the file paths from the torrent list
-    torrent_files = set()
+    # Extract the folder names from active torrents in qBittorrent
+    active_folders = set()
     for torrent in torrents:
-        for file in torrent.files:
-            torrent_files.add(file.name)  # Add the file path to the set
+        torrent_name = os.path.basename(torrent.name)
+        active_folders.add(torrent_name)
 
-    return torrent_files
+    return active_folders
 
-def delete_files_and_folders_not_in_qbittorrent(torrent_files, download_directory):
+def delete_folders_not_in_qbittorrent(active_folders, download_directory):
     for root, dirs, files in os.walk(download_directory, topdown=False):
-        for file in files:
-            file_path = os.path.join(root, file)
-            # Check if the file path is not in the torrent_files set
-            if file_path not in torrent_files:
-                print(f"Deleting file: {file_path}")
-                os.remove(file_path)
-        
         for folder in dirs:
-            folder_path = os.path.join(root, folder)
-            # Check if the folder path is not in the torrent_files set
-            if folder_path not in torrent_files:
+            folder_name = os.path.basename(folder)
+            # Check if the folder name is not in the active_folders set
+            if folder_name not in active_folders:
+                folder_path = os.path.join(root, folder)
                 print(f"Deleting folder: {folder_path}")
                 shutil.rmtree(folder_path)
 
@@ -45,16 +39,16 @@ if __name__ == "__main__":
     try:
         while True:
             print("Scanning downloads directory...")
-            
-            # Get the list of files in qBittorrent
-            torrent_files = get_qbittorrent_downloads()
 
-            print("Files found in qBittorrent:")
-            for file in torrent_files:
-                print(file)
+            # Get the list of active folders in qBittorrent
+            active_folders = get_active_qbittorrent_folders()
 
-            # Delete files and folders in the download directory not present in qBittorrent
-            delete_files_and_folders_not_in_qbittorrent(torrent_files, download_directory)
+            print("Active folders in qBittorrent:")
+            for folder in active_folders:
+                print(folder)
+
+            # Delete folders in the download directory not present in active qBittorrent torrents
+            delete_folders_not_in_qbittorrent(active_folders, download_directory)
 
             print("Cleanup completed successfully.")
 
